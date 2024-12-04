b700d66b81a4cba0a0bf87ee4d2f2d7552024be8
Naincy31 <iamnaincyrathore@gmail.com>
2024-12-04 11:15:43 +0530

    Completed development of Chips-Input using Class and HTML template

--- chips-input/index.js
+++ chips-input/index.js
@@ -1,16 +1,41 @@
 const form = document.getElementById('form')
-const inputEl = document.querySelector('input')
-const chipsContainer = document.querySelector('.chips')
+const userInput = document.getElementById('userInput')
+const chipTemplate = document.getElementById('chipTemplate')
+const chipsDisplay = document.querySelector('.chips-display')
 
-// const createChip = () => {
-//     const chipEl = document.createElement('div')
-//     chipEl.classList.add('chip')
-//     chipEl.textContent = inputEl.value
-//     const deleteBtn = document.createElement('button')
-//     deleteBtn.classList.add('delete')
-//     deleteBtn.textContent = 'X'
-//     chipEl.appendChild(deleteBtn)
-//     chipsContainer.appendChild(chipEl)
-// }
+class Chip {
+    constructor(name = '') {
+        const chip = chipTemplate.content.cloneNode(true)
+        this.chip = chip.querySelector('.chip')
+        this.chipName = this.chip.querySelector('.chip-name')
+        this.chip.querySelector('.remove').addEventListener('click', this.onRemove)
+        chipsDisplay.appendChild(this.chip)
+        this.setName(name)
+    }
 
-inputEl.addEventListener('input', createChip)
+    setName(name) {
+        this.chipName.textContent = name
+        name === '' ? (this.chip.style.display = 'none') : (this.chip.style.display = 'inherit')
+    }
+
+    finalizeName() {
+        this.chip.classList.add('finalized')
+    }
+
+    onRemove(e) {
+        e.target.parentElement.remove()
+    }
+}
+
+let chip = new Chip()
+
+userInput.addEventListener('input', e => {
+    chip.setName(e.target.value.trim())
+})
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    userInput.value = ''
+    chip.finalizeName()
+    chip = new Chip()
+})
